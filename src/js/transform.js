a261aa84d9c6bfbab4a199336263a4b640b54d94
Matthijs Wensveen <matthijs.wensveen@gmail.com>
2020-11-18 22:28:50 +0100

    Make Transform a class

--- src/js/transform.js
+++ src/js/transform.js
@@ -23,74 +23,87 @@
 
 /* global Matrix $M $V */
 
-function Transform() {
-	this.reset();
+class Transform {
+	constructor() {
+		this.reset();
+	}
+
+	reset() {
+		this.m = Matrix.I(3);
+	}
+
+	save() {
+		if (!this.stack) {
+			this.stack = [];
+		}
+		this.stack.push(this.m.dup());
+	}
+
+	restore() {
+		if (this.stack && this.stack.length > 0) {
+			this.m = this.stack.pop();
+		}
+	}
+
+	rotate(angle) {
+		var sin = Math.sin(angle);
+		var cos = Math.cos(angle);
+		this.m = this.m.multiply($M([
+			[cos, -sin, 0],
+			[sin, cos, 0],
+			[0, 0, 1]
+		]));
+	}
+
+	translate(x, y) {
+		this.m = this.m.multiply($M([
+			[1, 0, x],
+			[0, 1, y],
+			[0, 0, 1]
+		]));
+	}
+
+	scale(x, y) {
+		this.m = this.m.multiply($M([
+			[x, 0, 0],
+			[0, y, 0],
+			[0, 0, 1]
+		]));
+	}
+
+	transform(m11, m12, m21, m22, dx, dy) {
+		this.m = this.m.multiply($M([
+			[m11, m21, dx],
+			[m12, m22, dy],
+			[0, 0, 1]
+		]));
+	}
+
+	setTransform(m11, m12, m21, m22, dx, dy) {
+		this.m = $M([
+			[m11, m21, dx],
+			[m12, m22, dy],
+			[0, 0, 1]
+		]);
+	}
+
+	applyToContext(ctx) {
+		ctx.setTransform(this.m.e(1, 1), this.m.e(2, 1), this.m.e(1, 2), this.m.e(2, 2), this.m.e(1, 3), this.m.e(2, 3));
+	}
+
+	getTransformedPoint(x, y) {
+		var point = this.m.multiply($V([x, y, 1]));
+		return [point.e(1), point.e(2)];
+	}
 }
 
-Transform.prototype.reset = function() {
-	this.m = Matrix.I(3);
-};
 
-Transform.prototype.save = function() {
-	if (!this.stack) {
-		this.stack = [];
-	}
-	this.stack.push(this.m.dup());
-};
 
-Transform.prototype.restore = function() {
-	if (this.stack && this.stack.length > 0) {
-		this.m = this.stack.pop();
-	}
-};
 
-Transform.prototype.rotate = function(angle) {
-	var sin = Math.sin(angle);
-	var cos = Math.cos(angle);
-	this.m = this.m.multiply($M([
-		[cos, -sin, 0],
-		[sin,  cos, 0],
-		[   0,   0, 1]
-	]));
-};
 
-Transform.prototype.translate = function(x, y) {
-	this.m = this.m.multiply($M([
-		[1, 0, x],
-		[0, 1, y],
-		[0, 0, 1]
-	]));
-};
 
-Transform.prototype.scale = function(x, y) {
-	this.m = this.m.multiply($M([
-		[x, 0, 0],
-		[0, y, 0],
-		[0, 0, 1]
-	]));
-};
 
-Transform.prototype.transform = function(m11, m12, m21, m22, dx, dy) {
-	this.m = this.m.multiply($M([
-		[m11, m21, dx],
-		[m12, m22, dy],
-		[  0,   0,  1]
-	]));
-};
 
-Transform.prototype.setTransform = function(m11, m12, m21, m22, dx, dy) {
-	this.m = $M([
-		[m11, m21, dx],
-		[m12, m22, dy],
-		[  0,   0,  1]
-	]);
-};
 
-Transform.prototype.applyToContext = function(ctx) {
-	ctx.setTransform(this.m.e(1,1), this.m.e(2,1), this.m.e(1,2), this.m.e(2,2), this.m.e(1,3), this.m.e(2,3));
-};
 
-Transform.prototype.getTransformedPoint = function(x, y) {
-	var point = this.m.multiply($V([x, y, 1]));
-	return [point.e(1), point.e(2)];
-};
+
